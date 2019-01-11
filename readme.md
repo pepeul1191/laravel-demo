95272c7af41feba6a0d3b06849e10c4d583fedc3
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-11 10:29:40 -0500

    Acceso de BD con ParisORM

--- readme.md
+++ readme.md
@@ -1,8 +1,13 @@
 ### Laravel
 
+Crear modelos:
+
+    $ php artisan make:model models/Flight
+
 ---
 
 Fuentes:
 
 + https://stackoverflow.com/questions/2003101/zend-and-htaccess
 + https://stackoverflow.com/questions/21461565/creating-an-initializer-in-laravel
++ https://laravel.com/docs/5.7/eloquent
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,36 @@
 |
 */
 
+use App\Models\Branch;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('foo', function () {
     return 'Hello World';
+});
+
+Route::get('branch/list', function () {
+  $rpta = '';
+  $status = 200;
+  try {
+    $rs = \Model::factory('\App\Models\Branch', 'competition')
+      ->order_by_asc('id')
+      ->find_array();
+    $rpta = json_encode($rs);
+  }catch (\Exception $e) {
+    $status = 500;
+    $rpta = json_encode(
+      [
+        'tipo_mensaje' => 'error',
+        'mensaje' => [
+          'No se ha podido listar las sedes',
+          $e->getMessage()
+        ]
+      ]
+    );
+  }
+  return response($rpta, $status)
+    ->header('Content-Type', 'text/plain');
 });
